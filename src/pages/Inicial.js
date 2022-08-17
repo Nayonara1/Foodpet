80f2c487f3a69f803108db6df6abfca898667391
nayonara <nayonaragalvao@gmail.com>
2022-08-16 22:53:11 -0300

    atualizando

--- src/pages/Inicial.js
+++ src/pages/Inicial.js
@@ -3,7 +3,9 @@ import { View, Button, Text } from 'react-native';
 
 export default ({ navigation }) => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'   , }}>
-    <Text>Bem-vindo ao foodpet</Text>
+    <Text>Bem-vindo ao FoodPet!</Text>
+    
+    <Text>App para alimentar seu animail.</Text>
     <Button 
       title="Principal"
       
